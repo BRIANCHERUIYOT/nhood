2b7772519705401abb024cc3eb00f5cce79d04a9
BRIANCHERUIYOT <briancheruiyot@student.moringaschool.com>
2021-12-30 19:59:13 +0300

    adds app to settings

--- prhood/settings.py
+++ prhood/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
-    'aphood'
+    'users',
+    'aphood',
 ]
 
 MIDDLEWARE = [
