fa9733cc505e5bc55303c077b5c7d33506e59b90
BRIANCHERUIYOT <briancheruiyot@student.moringaschool.com>
2022-01-05 14:57:09 +0300

    adds forms.py

--- prhood/urls.py
+++ prhood/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', user_view.register, name='registration'),
-    path('', include('hood.urls')),
+    path('', include('aphood.urls')),
 ]
