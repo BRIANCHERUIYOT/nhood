ee0f9a1b48d6c1432ee7708682ff32f31d94a07e
BRIANCHERUIYOT <briancheruiyot@student.moringaschool.com>
2022-01-05 14:59:31 +0300

    adds users views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib import messages
+from .forms import UserRegistrationForm
+
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration.html', {"form": form})
